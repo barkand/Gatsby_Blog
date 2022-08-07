8a120d04bc352201f7410e4610cf73c0b5cad724
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2022-08-07 13:12:46 +0430

    Use env for pathPrefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     siteUrl: `https://RiskLab.ai`,
     title: "RiskLab AI",
   },
-  pathPrefix: "/RiskLab_Gatsby",
+  pathPrefix: process.env.REACT_APP_PATH_PREFIX,
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
