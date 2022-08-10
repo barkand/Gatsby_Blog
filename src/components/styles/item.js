aaa5f7571b233ed26f7838b7a9e37dd65d2c5d39
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2022-08-11 00:47:27 +0430

    Add Numbers of Column arg to Grid

--- src/components/styles/item.js
+++ src/components/styles/item.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Grid } from "@mui/material";
 
-export default function CustomItem({ children }) {
+export default function CustomItem({ children, columns = 2 }) {
   return (
-    <Grid item sm={11} md={6}>
+    <Grid item sm={12} md={12 / columns}>
       {children}
     </Grid>
   );
