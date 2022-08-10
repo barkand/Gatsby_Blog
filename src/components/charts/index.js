26825d664363a046466a2104c11b691ab6b7766d
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2022-08-11 00:47:49 +0430

    Add Scatter & Line to index

--- src/components/charts/index.js
+++ src/components/charts/index.js
@@ -1,3 +1,5 @@
 export { default as Figure } from "./figure/figure";
 export { default as StackChart } from "./stackChart";
+export { default as LineChart } from "./lineChart";
 export { default as MultilineChart } from "./multilineChart";
+export { default as ScatterChart } from "./scatterChart";
