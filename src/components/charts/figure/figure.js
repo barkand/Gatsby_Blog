ec2bb3e10dd1d773d4616c63cb50c929a1d5c331
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2022-08-10 16:04:51 +0430

    Add Default Message for Figures alt

--- src/components/charts/figure/figure.js
+++ src/components/charts/figure/figure.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
-const Figure = ({ source, size = 600, caption }) => {
+const Figure = ({ source, size = 600, caption = "" }) => {
   if (typeof window !== `undefined`) {
     size = window.innerWidth > size ? size : window.innerWidth - 80;
   }
