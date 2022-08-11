d728bd35a9336987932af508a37ac73ff6cadf55
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2022-08-11 17:15:11 +0430

    set margin

--- src/blog/ambiguity-in-the-financial-markets/charts/differential/chart.js
+++ src/blog/ambiguity-in-the-financial-markets/charts/differential/chart.js
@@ -17,7 +17,12 @@ const ChartDifferential = () => (
       }
     `}
     render={(data) => (
-      <ScatterChart data={[...data.allDifferentialCsv.nodes]} isDate={false} />
+      <ScatterChart
+        data={[...data.allDifferentialCsv.nodes]}
+        isDate={false}
+        size={{ width: 300, height: 300 }}
+        margin={{ top: 30, right: 0, bottom: 30, left: 300 }}
+      />
     )}
   />
 );
